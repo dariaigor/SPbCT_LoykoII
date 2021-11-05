0e3c2a33105f19fa164d8b73c21c685a21e3ac3f
dariaigor <92259900+dariaigor@users.noreply.github.com>
2021-11-05 11:00:52 +0300

    Add files via upload

---
+++ laba_29/laba_29.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, ".1251");
+	float a, b, c, P;
+	cout << "\n a = "; cin >> a;
+	cout << "\n b = "; cin >> b;
+	cout << "\n c = "; cin >> c;
+	if(a + b > c && a + c > b && b + c > a)
+	{
+		P = a + b + c;
+		cout << "\n Периметр треугольника = " << P;
+	}
+	else cout<< "\n Неверные исходные данные";
+}
